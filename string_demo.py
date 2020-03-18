e54a5a8d18692cf0014bcc5560ce6e4b66a9c365
Remy <rjiang2@babson.com>
2020-03-18 13:32:37 -0400

    Create string_demo.py

---
+++ string_demo.py
@@ -0,0 +1,19 @@
+print(2 - 1)
+# print('2' - '1')
+first_name = 'Smit'
+last_name = 'Patel'
+name = first_name + '' + last_name
+print(name)
+
+print('Naah, na na nanana naah, nanana naah, hey Jude. \n' *10)
+
+print(f'Hello, {name}!')
+
+actor = 'Joaquin Phoenix'
+year = 2020
+movie = 'Joker'
+print(f'{actor} wins Best Actor for {movie}')
+
+pi = 3.14159265358
+
+print(f'Pi equals{pi:.3}')
