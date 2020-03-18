7491ee2754f71f9196919cb4f92fa4e7e8fa61e0
Remy <rjiang2@babson.com>
2020-03-18 12:57:57 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print('hello, world')
+# print('hello, world')
+
+# print(2 + 2)
+
+name = input('Please enter your name: ')
+
+print('Hello,', name)
+
+grade = 'A'
+
+print(name, 'got a grade', grade)
+
+a = 123
+print(a)
+
+a = 'ABC'
+# Whatever inside the '' is a string. String is the most used.
+print(a)
+
+x = 10
+x = x + 2
+print(x)
+x += 2
+print(x)
