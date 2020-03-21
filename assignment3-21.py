bd8a869620a6e6af8082016616a75cd8ff1cdc0e
Remy <rjiang2@babson.com>
2020-03-21 18:57:33 -0400

    Update assignment3-21.py

--- assignment3-21.py
+++ assignment3-21.py
@@ -1,10 +1,15 @@
 import math
 def quadratic(a, b, c):
-    x1 = (-b+sqrt(b*b-4*a*c))/(2*a)
-    x2 = (-b-sqrt(b*b-4*a*c))/(2*a)
-    return x1, x2
+    if b*b >= 4*a*c:
+        x1 = (-b+math.sqrt(b*b-4*a*c))/(2*a)
+        x2 = (-b-math.sqrt(b*b-4*a*c))/(2*a)
+        return x1, x2
+    else:
+        return None
 
-
+# for test purpose
+x1, x2 = quadratic(1, 3, 2)
+print(x1, x2)
 
 
     
